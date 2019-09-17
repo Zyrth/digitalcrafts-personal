78b3af52d291acaac6952b7171af0b61eb219872
Jaclyn Legacion <jglegacion@gmail.com>
2019-09-17 13:18:55 -0500

    Complete level3 JS exercises

--- week4/2-tuesday/lab/j-ckie/js101/exercises/level3.js
+++ week4/2-tuesday/lab/j-ckie/js101/exercises/level3.js
@@ -1,25 +1,36 @@
 function lvl3exercise1 () {
     // Create a "hello" and a "world", return the concatenation of the two
+    var variableHello = "hello";
+    var variableWorld = "world";
+    return variableHello + variableWorld;
   
   }
   
   function lvl3exercise2 () {
     // Create a "hello" and a 12, return the concatenation of the two
-  
+    var variableHello = "hello";
+    var variableNumber = 12;
+    return variableHello + variableNumber;
   }
   
   function lvl3exercise3 () {
     // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-  
+    var variableNumber = 12;
+    var variableString = variableNumber.toString();
+    return variableString;
   }
   
   function lvl3exercise4 () {
     // Create a "hello world!" string. Return the length of the string
-  
+    var helloWorld = "hello world!";
+    return helloWorld.length;
   }
   
   function lvl3exercise5 () {
     // Create a "hello world!" string. Return the index of the word "world".
-  
+    var helloWorld = "hello world!";
+    var indexWorld = helloWorld.indexOf("world");
+    return indexWorld;
   }
+  
   
