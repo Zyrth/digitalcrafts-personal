6916de43cd95256d2ae63fe09117c0e78ce48ed8
Jaclyn Legacion <jglegacion@gmail.com>
2019-09-23 12:08:26 -0500

    Add event listener to buttons

--- week5/1-monday/lab/j-ckie/blackjack/blackjack.js
+++
@@ -1,6 +0,0 @@
-//this is where the js goes//
-
-function popUp() {
-    var popup = documents.getElementById("rules-popup");
-    popUp.classList.toggle("show");
-}
---
+++ week5/1-monday/lab/j-ckie/blackjack/script/blackjack.js
@@ -0,0 +1,13 @@
+//this is where the js goes//
+
+document.getElementById("btnDeal").addEventListener("click", function(){
+    // deal logic
+}); 
+
+document.getElementById("btnHit").addEventListener("click", function(){
+    // hit logic
+});
+
+document.getElementById("btnStand").addEventListener("click", function(){
+    // stand logic
+});
