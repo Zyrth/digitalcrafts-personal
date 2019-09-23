6f6177241f0902a3015c12a6d51b4755a8863fb5
Jaclyn Legacion <jglegacion@gmail.com>
2019-09-23 09:51:22 -0500

    add functions exercise

---
+++ week4/4-thursday/lab/j-ckie/functions/functions.js
@@ -0,0 +1,9 @@
+// Implement all of the following problems without using a while or a for loop unless you were explicitly asked to use loops.
+
+// Positive Numbers
+// Write a function which takes an array of numbers as input and returns a new array containing only the positive numbers in the given array.
+
+function positiveNumber(myArray) {
+    var newArray = [];
+    
+}
