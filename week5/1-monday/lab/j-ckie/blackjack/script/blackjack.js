dc66b05cb239fc51a718ca156e6cd70e0356f16b
Jaclyn Legacion <jglegacion@gmail.com>
2019-09-24 09:08:44 -0500

    Add DOM exercises

--- week5/1-monday/lab/j-ckie/blackjack/script/blackjack.js
+++ week5/1-monday/lab/j-ckie/blackjack/script/blackjack.js
@@ -1,2 +1,20 @@
 //this is where the js goes//
 
+document.getElementById("btnDeal").addEventListener("click", function(){
+    // deal logic
+    window.alert("1")
+}); 
+
+document.getElementById("btnHit").addEventListener("click", function(){
+    // hit logic
+    window.alert("test");
+});
+
+document.getElementById("btnStand").addEventListener("click", function(){
+    // stand logic
+});
+
+function myPopUp() {
+    var popup = document.getElementById("btnDeal");
+    window.alert("1");
+}
